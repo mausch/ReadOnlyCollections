e882436f1f864121440fe8655d23f547d797c541
mausch <mauricioscheffer@gmail.com>
2012-09-11 20:14:21 -0300

    initial commit

---
+++ ReadOnlyCollections/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace System.Collections.Generic {
+    public interface IReadOnlyCollection<out T> : IEnumerable<T> {
+        int Count { get; }
+    }
+
+    public interface IReadOnlyList<out T> : IReadOnlyCollection<T> {
+        T this[int index] { get; }
+    }
+
+    public interface IReadOnlyDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey, TValue>> {
+        TValue this[TKey key] { get; }
+        IEnumerable<TKey> Keys { get; }
+        IEnumerable<TValue> Values { get; }
+    }
+}
