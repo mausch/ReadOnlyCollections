b0434bd373dd44abf1b622ec7ccb15b25e3d595b
mausch <mauricioscheffer@gmail.com>
2012-09-11 23:55:08 -0300

    Start multitargeting

--- ReadOnlyCollectionsInterfaces/Interfaces.cs
+++ ReadOnlyCollectionsInterfaces/Interfaces.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 
 namespace System.Collections.Generic {
+#if NET40
     public interface IReadOnlyCollection<out T> : IEnumerable<T> {
+#elif NET20 || NET35
+    public interface IReadOnlyCollection<T> : IEnumerable<T> {
+#endif
         int Count { get; }
     }
-
+#if NET40
     public interface IReadOnlyList<out T> : IReadOnlyCollection<T> {
+#elif NET20 || NET35
+    public interface IReadOnlyList<T> : IReadOnlyCollection<T> {
+#endif
         T this[int index] { get; }
     }
 
---
+++ build.ps1
@@ -0,0 +1,4 @@
+$versions = ("v2.0", "v4.0", "v4.5")
+foreach ($v in $versions) {
+	msbuild /m readonlyCollections.sln /p:TargetFrameworkVersion=$v /p:Configuration=Release
+}
