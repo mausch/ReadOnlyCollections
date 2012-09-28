30c8e275a51b38a58bcc269526999a0cf598c836
mausch <mauricioscheffer@gmail.com>
2012-09-28 19:52:35 -0300

    Abort build on error

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
+$ErrorActionPreference = "Stop"
 $versions = ("v2.0", "v3.5", "v4.0", "v4.5")
 foreach ($v in $versions) {
 	"================="
 	"Building for " + $v
 	"================="
 	msbuild /m ReadOnlyCollections.sln /p:TargetFrameworkVersion=$v /p:Configuration=Release
+	if ($LASTEXITCODE -ne 0) { throw "error" }
 }
