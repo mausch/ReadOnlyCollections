22b7f19cdd860476ae32ad55793408f0e9f824a7
mausch <mauricioscheffer@gmail.com>
2012-09-28 18:26:33 -0300

    Add missing methods

--- ReadOnlyCollectionsInterfaces/Interfaces.cs
+++ ReadOnlyCollectionsInterfaces/Interfaces.cs
@@ -20,5 +20,7 @@ namespace System.Collections.Generic {
         TValue this[TKey key] { get; }
         IEnumerable<TKey> Keys { get; }
         IEnumerable<TValue> Values { get; }
+        bool ContainsKey(TKey key);
+        bool TryGetValue(TKey key, out TValue value);
     }
 }
