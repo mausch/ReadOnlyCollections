0ec2c0c8e86656ea438fab1aa09c8ad77ef1af82
mausch <mauricioscheffer@gmail.com>
2012-09-29 13:42:32 -0300

    multitargeting adjustments

--- build.ps1
+++ build.ps1
@@ -1,4 +1,7 @@
-$versions = ("v2.0", "v4.0", "v4.5")
+$versions = ("v2.0", "v3.5", "v4.0", "v4.5")
 foreach ($v in $versions) {
+	"================="
+	"Building for " + $v
+	"================="
 	msbuild /m ReadOnlyCollections.sln /p:TargetFrameworkVersion=$v /p:Configuration=Release
 }
