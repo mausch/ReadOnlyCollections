404360b4048d13cec8cbeeae36e3308106f70998
Andrew Lock <andrewlock.net@gmail.com>
2017-09-07 09:12:12 +0100

    Update to support netstandard1.0 and netstandard 2.0
    Supporting 2.0 explicitly is not necessry, but avoids using shim explictly
    Updated to new SDK style csproj
    Moved nuspec file properties into csproj to allow packaging with dotnet pack
    Renamed symbols - anything pre-net45 is LEGACY

--- build.ps1
+++ build.ps1
@@ -1,11 +1,2 @@
 $ErrorActionPreference = "Stop"
-$versions = ("v2.0", "v3.5", "v4.0", "v4.5")
-foreach ($v in $versions) {
-	"================="
-	"Building for " + $v
-	"================="
-	msbuild /m ReadOnlyCollections.sln /p:TargetFrameworkVersion=$v /p:Configuration=Release
-	if ($LASTEXITCODE -ne 0) { throw "error" }
-}
-.nuget\nuget.exe pack ReadOnlyCollectionsInterfaces.nuspec
-.nuget\nuget.exe pack ReadOnlyCollectionsExtensions.nuspec
+dotnet pack -c Release -o ..\.nuget
