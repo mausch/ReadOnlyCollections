29a6948698526657df76fe6c1cd736ed659ef826
Mauricio Scheffer <mauricioscheffer@gmail.com>
2022-06-05 16:56:14 +0100

    Test Release since the script builds Release

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = "Stop"
 dotnet build -c Release
 echo "Testing .NET 3.5"
-dotnet run --project Tests\Tests.csproj --framework net35
+dotnet run -c Release --project Tests\Tests.csproj --framework net35
 echo "Testing .NET 4.6.2"
-dotnet run --project Tests\Tests.csproj --framework net462
+dotnet run -c Release --project Tests\Tests.csproj --framework net462
 echo "Testing .NET 5.0"
-dotnet run --project Tests\Tests.csproj --framework net5.0
+dotnet run -c Release --project Tests\Tests.csproj --framework net5.0
 echo "Testing .NET 6.0"
-dotnet run --project Tests\Tests.csproj --framework net6.0
+dotnet run -c Release --project Tests\Tests.csproj --framework net6.0
