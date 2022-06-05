271dbce7bfdab4d54d33dfb089545b5a43d7e434
Mauricio Scheffer <mauricioscheffer@gmail.com>
2022-06-05 12:39:02 +0100

    Some tests

---
+++ Tests/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    static class Startup
+    {
+        public static int Main(string[] args)
+        {
+            var tests = new Tests();
+            tests.AsReadOnlySet();
+            tests.ToReadOnlySet();
+            Console.WriteLine("All tests passed!");
+            return 0;
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using ReadOnlyCollectionsExtensions;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void AsReadOnlySet()
+        {
+            var set = new HashSet<int>(new[] { 1, 2, 3 });
+            IReadOnlySet<int> readonlySet = set.AsReadOnlySet();
+            CollectionAssert.AreEqual(expected: set, actual: readonlySet);
+        }
+
+        [Test]
+        public void ToReadOnlySet()
+        {
+            var set = new[] { 1, 2, 3 };
+            IReadOnlySet<int> readonlySet = set.ToReadOnlySet();
+            CollectionAssert.AreEqual(expected: set, actual: readonlySet);
+        }
+    }
+}
