d80f319b24d407a6ccaa3fa518fc1261067a8503
Mauricio Scheffer <mauricioscheffer@gmail.com>
2022-06-05 16:56:22 +0100

    Test .NET 4.8

--- build.ps1
+++ build.ps1
@@ -4,6 +4,8 @@ echo "Testing .NET 3.5"
 dotnet run -c Release --project Tests\Tests.csproj --framework net35
 echo "Testing .NET 4.6.2"
 dotnet run -c Release --project Tests\Tests.csproj --framework net462
+echo "Testing .NET 4.8"
+dotnet run -c Release --project Tests\Tests.csproj --framework net48
 echo "Testing .NET 5.0"
 dotnet run -c Release --project Tests\Tests.csproj --framework net5.0
 echo "Testing .NET 6.0"
